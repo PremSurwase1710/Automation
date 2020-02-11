cba4ddc9e50b8c6c67cf5718c0f8b67ef1efe7e3
Prem Surwase <surwase.prem17@gmail.com>
2020-02-11 00:11:52 -0500

    Updated Commit_Test_2.sh

--- Commit_Test_2.sh
+++ Commit_Test_2.sh
@@ -1,1 +1,31 @@
-This is a new test.
+#!/bin/bash
+a=2;
+
+echo "Hello World"
+
+while true
+do
+
+   echo "============================"
+   echo "Select the below options"
+   echo "1. To read one, press 1"
+   echo "2. To read two, press 2"
+   echo "0. Exit"
+   echo "============================"
+   read value
+   case $value in 
+      1)
+      echo "This is one"
+      ;;
+      2) 
+      echo "This is two"
+      ;;
+      0)
+      echo "Exiting from the game"
+      exit 0
+      ;;
+      *)
+      echo "This is done"
+      ;;
+   esac
+done
